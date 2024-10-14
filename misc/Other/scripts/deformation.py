97f615af33c72073af097ee349dd4bd8af5ef3f0
LiuYuML <165549538+LiuYuML@users.noreply.github.com>
2024-10-14 22:44:38 +0800

    Update deformation.py

--- misc/Other/scripts/deformation.py
+++ misc/Other/scripts/deformation.py
@@ -187,79 +187,10 @@ def main(image_folder, base_output_path, dataset_name, gt_path,gt_name,index,thr
             print(output_file_path," Already Done!")
 # Example usage:
 if __name__ == "__main__":
-    #image_folder = r'C:\Users\leoyu\Desktop\OTBSpace\SOTDrawRect\OTB100'
-    #base_output_path = r'C:\Users\leoyu\Desktop\LeoSOTSpace\NTB\utils\deformation'
-    #dataset_name = 'otb'
-    #index = -3  # You can specify the desired index here
-    #gt_path = r"C:\Users\leoyu\Desktop\OTBSpace\SOTDrawRect\OTB100"
+    #image_folder = r'<path-to-dataset>\OTB100'
+    #base_output_path = r'<output_path>\deformation'
+    #dataset_name = 'otb' # 'avist' 'got10k_test' 'VOT'
+    #index = -3  # if not work, try -2
+    #gt_path = r"<path-to-dataset>\OTB100"
     #gt_name = r"groundtruth_rect.txt"
     #main(image_folder, base_output_path, dataset_name,gt_path,gt_name,index)
-
-    #image_folder = r'C:\Users\leoyu\Desktop\LeoSOTSpace\avist\avist\sequences'
-    #base_output_path = r'C:\Users\leoyu\Desktop\LeoSOTSpace\NTB\utils\deformation'
-    #dataset_name = 'avist'
-    #index = -2  # You can specify the desired index here
-    #gt_path = r"C:\Users\leoyu\Desktop\LeoSOTSpace\avist\avist\anno"
-    #gt_name = r"groundtruth_rect.txt"
-    #main(image_folder, base_output_path, dataset_name,gt_path,gt_name,index)
-    
-    #image_folder = r'C:\Users\leoyu\Desktop\LeoSOTSpace\GOT-10k\GOT-10k\test'
-    #base_output_path = r'C:\Users\leoyu\Desktop\LeoSOTSpace\NTB\utils\deformation'
-    #dataset_name = 'got10k_test'
-    #index = -2  # You can specify the desired index here
-    #gt_path = r"C:\Users\leoyu\Desktop\LeoSOTSpace\GOT-10k\best_got_results\garbage_submission_2023_05_21_13_11_04"
-    #gt_name = r"_001.txt"
-    #main(image_folder, base_output_path, dataset_name,gt_path,gt_name,index)
-    
-    #image_folder = r'C:\Users\leoyu\Desktop\LeoSOTSpace\NTB\test\sequences'
-    #base_output_path = r'C:\Users\leoyu\Desktop\LeoSOTSpace\NTB\utils\deformation'
-    #dataset_name = 'XJU211'
-    #index = -2  # You can specify the desired index here
-    #gt_path = r"C:\Users\leoyu\Desktop\LeoSOTSpace\NTB\test\anno"
-    #gt_name = r"groundtruth_rect.txt"
-    #main(image_folder, base_output_path, dataset_name,gt_path,gt_name,index)
-    
-    
-    
-    #image_folder = r'D:\Documents\Desktop\ly\MBZUAI\Dataset CVPR\VOT2018'
-    #base_output_path = r'D:\Documents\Desktop\ly\MBZUAI\Dataset CVPR\deformation'
-    #dataset_name = 'VOT'
-    #index = -2  # You can specify the desired index here
-    #gt_path = r"D:\Documents\Desktop\ly\MBZUAI\Dataset CVPR\VOT2018"
-    #gt_name = r"groundtruth.txt"
-    #main(image_folder, base_output_path, dataset_name,gt_path, gt_name,index)
-    
-    
-    #image_folder = r'D:\Documents\Desktop\ly\MBZUAI\Dataset CVPR\VOT2018'
-    #base_output_path = r'D:\Documents\Desktop\ly\MBZUAI\Dataset CVPR\camera_motion'
-    #dataset_name = 'VOT'
-    #index = -2  # You can specify the desired index here
-    #gt_path = r"D:\Documents\Desktop\ly\MBZUAI\Dataset CVPR\VOT2018"
-    #gt_name = r"groundtruth.txt"
-    #main(image_folder, base_output_path, dataset_name, index)
-    
-    
-    #image_folder = r'E:\ly\ACCV\dataset\Other night benchmark\UAVDark135\UAVDark135_TSP_out\Sequences'
-    #base_output_path = r'D:\Documents\Desktop\ly\MBZUAI\Dataset CVPR\codes\tmp\deformation'
-    #dataset_name = 'UAVDark135'
-    #index = -2  # You can specify the desired index here
-    #gt_path = r"E:\ly\ACCV\dataset\Other night benchmark\UAVDark135\UAVDark135_TSP_out\anno_revise"
-    #gt_name = r"groundtruth.txt"
-    #main(image_folder, base_output_path, dataset_name,gt_path, gt_name,index)
-    
-    
-    #image_folder = r'E:\ly\ACCV\dataset\Other night benchmark\NAT2021\NAT2021_test\NAT2021L\data_seq'
-    #base_output_path = r'D:\Documents\Desktop\ly\MBZUAI\Dataset CVPR\codes\tmp\deformation'
-    #dataset_name = 'NAT2021'
-    #index = -2  # You can specify the desired index here
-    #gt_path = r"E:\ly\ACCV\dataset\Other night benchmark\NAT2021\NAT2021_test\NAT2021L\anno"
-    #gt_name = r"groundtruth.txt"
-    #main(image_folder, base_output_path, dataset_name,gt_path, gt_name,index)
-    
-    image_folder = r'E:\ly\ACCV\dataset\Other night benchmark\DarkTrack\DarkTrack2021\data_seq'
-    base_output_path = r'D:\Documents\Desktop\ly\MBZUAI\Dataset CVPR\codes\tmp\deformation'
-    dataset_name = 'DarkTrack2021'
-    index = -2  # You can specify the desired index here
-    gt_path = r"E:\ly\ACCV\dataset\Other night benchmark\DarkTrack\DarkTrack2021\anno"
-    gt_name = r"groundtruth.txt"
-    main(image_folder, base_output_path, dataset_name,gt_path, gt_name,index)
